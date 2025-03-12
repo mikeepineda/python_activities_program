fd1997f75ed53ca33fb1fc7a315ad11dc3fa49d1
incessmikee <incess.mikee@gmail.com>
2025-03-12 22:33:36 +0800

    code complete

--- batch_1/prog_1_print_bigger_number.py
+++ batch_1/prog_1_print_bigger_number.py
@@ -1,4 +1,13 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # ask user for two numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
 # print bigger number
+if num1>num2: 
+    print ("The bigger number is" , num1)
+elif num2>num2:
+    print ("The bigger number is" , num2)
+else: 
+    print ("Both numbers are the same value.")
