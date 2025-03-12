66edf3cafa9f108e126c4bd2f97644f27e7c2bbd
incessmikee <incess.mikee@gmail.com>
2025-03-12 22:11:44 +0800

    Create file

---
+++ batch_1/prog_1_print_bigger_number.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
