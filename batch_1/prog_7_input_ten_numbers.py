1e4122cbe9f08ddf2bc074b7fda7a1a1cf97b51b
incessmikee <incess.mikee@gmail.com>
2025-03-13 01:58:11 +0800

    code complete

--- batch_1/prog_7_input_ten_numbers.py
+++ batch_1/prog_7_input_ten_numbers.py
@@ -1,5 +1,11 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # ask user to input ten numbers
+total = 0 
+
+for i in range (10):
+    num = float(input(f"Enter your number {i+1}: "))
+    total += num
 
 # print output
+print("The sum of all numbers is : ", total)
