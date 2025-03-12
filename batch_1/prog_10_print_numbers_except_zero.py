4f2abcdcf8872e7be8e29f26c8bd17e77f7e9d67
incessmikee <incess.mikee@gmail.com>
2025-03-13 02:13:45 +0800

    prog 10 pseudocode

---
+++ batch_1/prog_10_print_numbers_except_zero.py
@@ -0,0 +1,8 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# set range up to 100 only
+
+# check if num is divisible by 10
+
+# print num
+    
