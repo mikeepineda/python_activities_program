41cae493d4dc0d66c61750e52a783941a04b36e9
incessmikee <incess.mikee@gmail.com>
2025-03-12 23:00:27 +0800

    Created file

---
+++ batch_1/prog_3_print_sum.py
@@ -0,0 +1,9 @@
+# Prog03:Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+# ask two numbers
+num1 = int(input ("Enter first number: "))
+num2 = int(input ("Enter second number: "))
+result = num1 + num2
+
+# print sum
+print (num1, "+", num2, "=", result)
