9c171d61e503693a0e54123158557b218f27c527
incessmikee <incess.mikee@gmail.com>
2025-03-12 23:27:47 +0800

    code complete

--- batch_1/prog_4_print_product.py
+++ batch_1/prog_4_print_product.py
@@ -1,5 +1,9 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 # ask user two numbers 
+num1 = float(input ("Enter first number: "))
+num2 = float(input ("Enter second number: "))
+result = num1 * num2
 
 # print product
+print (num1, "x", num2, "=", result)
