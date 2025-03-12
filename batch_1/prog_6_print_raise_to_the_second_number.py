b54a34b21f811cba907788dce7482317139369d1
incessmikee <incess.mikee@gmail.com>
2025-03-13 01:48:08 +0800

    prog 06 pseudocode

---
+++ batch_1/prog_6_print_raise_to_the_second_number.py
@@ -0,0 +1,3 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# ask user two input
+# print raise to the second number
