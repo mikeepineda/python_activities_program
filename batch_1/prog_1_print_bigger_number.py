d5cdc7a467b926fb044c26a4301925c1d4440aaa
incessmikee <incess.mikee@gmail.com>
2025-03-12 22:25:34 +0800

    batch 01: pseudocode

--- batch_1/prog_1_print_bigger_number.py
+++ batch_1/prog_1_print_bigger_number.py
@@ -1,4 +1,4 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
+# ask user for two numbers
+# print bigger number
