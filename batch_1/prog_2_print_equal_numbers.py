04ec9efda66ac06ae7e6f551a0d5a62f4912cad8
incessmikee <incess.mikee@gmail.com>
2025-03-12 22:47:39 +0800

    Code Complete

---
+++ batch_1/prog_2_print_equal_numbers.py
@@ -0,0 +1,11 @@
+# Prog02:Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# ask user two numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+# print equal numbers
+if num1 == num2:
+    print ("Equal")
+else: 
+    print ("Your numbers are not equal.")
