983dea341e1674870a15c571af214f1cf4a5176b
incessmikee <incess.mikee@gmail.com>
2025-03-13 02:03:04 +0800

    code complete

--- batch_1/prog_8_print_odd_numbers.py
+++ batch_1/prog_8_print_odd_numbers.py
@@ -1,5 +1,12 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers. 
 
 # ask user to input 10 numbers
+odd_count = 0
+
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    if num % 2 != 0:  
+        odd_count += 1
 
 # print odd numbers
+print("Total odd numbers:", odd_count)
