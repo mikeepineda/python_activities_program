e8f1a5c7eba0aada2e662b54d8d4e8325fd31099
incessmikee <incess.mikee@gmail.com>
2025-03-13 02:15:14 +0800

    code complete

--- batch_1/prog_10_print_numbers_except_zero.py
+++ batch_1/prog_10_print_numbers_except_zero.py
@@ -1,8 +1,7 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # set range up to 100 only
-
-# check if num is divisible by 10
-
-# print num
+for num in range (101):
+    if num % 10 != 0: # check if num is divisible by 10
+        print (num) # print num
     
