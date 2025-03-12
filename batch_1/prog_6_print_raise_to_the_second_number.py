8c03b60fd5b0ef2f318d2237d0b9bda3d89f53f7
incessmikee <incess.mikee@gmail.com>
2025-03-13 01:49:49 +0800

    code completed

--- batch_1/prog_6_print_raise_to_the_second_number.py
+++ batch_1/prog_6_print_raise_to_the_second_number.py
@@ -1,3 +1,8 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
 # ask user two input
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
 # print raise to the second number
+print ("Result = ", num1**num2)
