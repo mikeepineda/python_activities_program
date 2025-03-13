0e5235e9f35f2ed8c0ac62dbe6aa106d5b27ce2b
incessmikee <incess.mikee@gmail.com>
2025-03-13 17:54:55 +0800

    code exchange

--- batch_1/prog_5_print_quotient.py
+++ batch_1/prog_5_print_quotient.py
@@ -5,8 +5,4 @@ num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 
 # print quotient
-<<<<<<< HEAD
 print ("Quotient: ", num1/num2 if num2 != 0 else "Undefined")
-=======
-print ("Quotient: ", num1/num2 if num2 != 0 else "Undefined")
->>>>>>> e5b97e18679d25dc0500b970fd3532cd0add7fae
