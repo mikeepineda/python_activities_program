a7977816e533c5fb89093895aa3adb29f029ff51
incessmikee <incess.mikee@gmail.com>
2025-03-13 15:01:56 +0800

    prog 10 pseudocode

---
+++ batch_2/prog_10_print_numbers_between.py
@@ -0,0 +1,7 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask user two inputs
+# determine the range
+# find the smaller number and add 1, and find the larger number
+# loop 
+
