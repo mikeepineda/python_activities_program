133e77a7693666791b451f55055fc751dbb8cb1e
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:15:17 +0800

    add spaces: code complete

--- batch_2/prog_02_print_not_equal.py
+++ batch_2/prog_02_print_not_equal.py
@@ -2,8 +2,10 @@
 
 # ask user two inputs
 if int(input("Input your first number: ")) != int(input("Input your second number: ")):
+
 # print not equal if inputs are not equal
     print ("Your numbers are not equal.")
+
 # print equal if inputs are equal
 else: 
     print ("Your numbers are equal.")
