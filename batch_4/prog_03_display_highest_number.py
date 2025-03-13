9d4d26aa720e7ed89a6838e8419667cfffe93796
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:12:02 +0800

    ask uder's input, add loop

--- batch_4/prog_03_display_highest_number.py
+++ batch_4/prog_03_display_highest_number.py
@@ -1,6 +1,16 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
-# ask for user's input
+highest = None
+
 # create loop
-# break if the input is invalid
+while True:
+
+# ask for user's input    
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        if highest is None or num > highest:
+            highest = num
+    except ValueError:
+        break # break if the input is invalid
+    
 # display the highest number
