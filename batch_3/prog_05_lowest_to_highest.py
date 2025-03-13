d2b250fa40f0813fdb07afbce570a629445953eb
incessmikee <incess.mikee@gmail.com>
2025-03-13 19:26:08 +0800

    ask for user's input

--- batch_3/prog_05_lowest_to_highest.py
+++ batch_3/prog_05_lowest_to_highest.py
@@ -1,7 +1,10 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 # add list to store user input
+numbers = []
+
 # ask for user's input
+num = int(input("Enter a number: "))
 # store valid number
 # break if the input is invalid
 # display num from lowest to highest
