7427959dbd4a7f90f51f01a6ef57f2cdefe5d97d
incessmikee <incess.mikee@gmail.com>
2025-03-13 22:46:14 +0800

    add list for duplicate numbers, print display: code complete

--- batch_4/prog_01_display_duplicate_numbers.py
+++ batch_4/prog_01_display_duplicate_numbers.py
@@ -7,7 +7,13 @@ for i in range (10):
     num = int(input(f"Enter your number {i + 1}: "))
     numbers.append(num)
 
+# empty list for duplicate numbers 
+duplicate_numbers = []
+
 # find numbers that have duplicate
 for num in numbers: 
      if numbers.count(num) > 1 and num not in duplicate_numbers:
         duplicate_numbers.append(num) 
+
+# display result
+print ("Numbers with duplicate:", duplicate_numbers)
