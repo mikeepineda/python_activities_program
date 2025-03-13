9dba76228613cbaa375a9fa0487c61b58e69736a
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:34:07 +0800

    prog 07 pseudocode

---
+++ batch_2/prog_07_print_even_numbers.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# even number counter
+
+# ask the user to input 10 numbers
+
+# check if the number is even
+
+# print the count of even numbers
