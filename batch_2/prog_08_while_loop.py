0a2c9003b818cc2b02b8d7f3eeb35500b24c397a
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:43:58 +0800

    code complete

--- batch_2/prog_08_while_loop.py
+++ batch_2/prog_08_while_loop.py
@@ -1,5 +1,7 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# start from the first odd number
-# print the odd number
-# move to the next odd number
+num = 1 # start from the first odd number
+
+while num <= 100:
+    print(num)  # Print the odd number
+    num += 2 # move to the next odd number
