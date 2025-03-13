e0c4e148bfadedb060d8bf23890301e16b6b41b0
incessmikee <incess.mikee@gmail.com>
2025-03-13 18:40:20 +0800

    finalize: code complete

--- batch_3/prog_02_display_first_entry_if_duplicate.py
+++ batch_3/prog_02_display_first_entry_if_duplicate.py
@@ -2,14 +2,16 @@
 
 numbers = [] # store user input
 seen = set() # set to track seen numbers
+
 # ask user to input 10 numbers
 for i in range (10): 
-    num = int(input(f"Enter your number {i +1: }"))
+    num = int(input(f"Enter your number {i + 1}: "))
     if num not in seen: # check if number is already displayed
         seen.add(num)   # mark as seen
     numbers.append(num) # store in list
-# display result
-print("Numbers with only first occurrence of duplicates:")
+
+print()     # display result
+
 # create an empty set called displayed to track prints
 displayed = set()  # Another set to track what we print
 for num in numbers:
