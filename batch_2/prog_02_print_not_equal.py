f6ff7978b6f53dce30c468fb79cc7a48f46a936f
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:13:45 +0800

    code complete

--- batch_2/prog_02_print_not_equal.py
+++ batch_2/prog_02_print_not_equal.py
@@ -1,7 +1,9 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # ask user two inputs
-
+if int(input("Input your first number: ")) != int(input("Input your second number: ")):
 # print not equal if inputs are not equal
-
+    print ("Your numbers are not equal.")
 # print equal if inputs are equal
+else: 
+    print ("Your numbers are equal.")
