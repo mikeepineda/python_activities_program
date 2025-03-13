8e89949f093ab4b14854ebfda99649ca484edfbd
incessmikee <incess.mikee@gmail.com>
2025-03-13 18:52:19 +0800

    ask for user's input

--- batch_3/prog_03_display_unique_duplicate.py
+++ batch_3/prog_03_display_unique_duplicate.py
@@ -1,7 +1,9 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 # create a list to store user's input
+numbers = []
 # ask for user's input
+num = int(input("Enter a number:"))
 # check if num is a duplicate
 # store only unique numbers
 # exit loop if the input is invalid
