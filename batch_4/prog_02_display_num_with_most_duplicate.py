be171bd4948f5dd4b1229b76487ce8018c1c6491
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:01:34 +0800

    ask user's input

--- batch_4/prog_02_display_num_with_most_duplicate.py
+++ batch_4/prog_02_display_num_with_most_duplicate.py
@@ -1,8 +1,15 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 # import counter
+from collections import Counter
+
 # add empty list for user's input
+numbers = []
+
 # ask user's input
+num = int(input("Enter a number (or any non-number to stop): "))
+numbers.append(num)
+
 # break when the input is invalid
 # find the most frequent nummber
 # print display
