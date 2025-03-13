f9b414ab3c4f901ade7e890b3ba16c2e63e9dbd1
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:24:15 +0800

    prog 06 psuedocode

---
+++ batch_2/prog_06_input_ten_numbers.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# ask 10 numbers from user
+# compute the result
+# print the result
