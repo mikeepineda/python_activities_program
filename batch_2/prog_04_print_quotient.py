1a3579ba2e61d77a7d1b0777937d650337d24a80
incessmikee <incess.mikee@gmail.com>
2025-03-13 13:57:05 +0800

    code complete

--- batch_2/prog_04_print_quotient.py
+++ batch_2/prog_04_print_quotient.py
@@ -1,5 +1,8 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point.
 
 # ask user two inputs
+A = int(input ("Input your first number: "))
+B = int(input ("Input your second number: "))
 
 # print quotient of two inputs
+print (A // B if B != 0 else "Undefined")
