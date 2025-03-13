59d52ce9e854f8b66f5f498aa1133f22de48cf86
incessmikee <incess.mikee@gmail.com>
2025-03-13 22:53:21 +0800

    prog 02 pseudocode

---
+++ batch_4/prog_02_display_num_with_most_duplicate.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# add empty list for user's input
+# ask user's input
+# break when the input is invalid
+# find the most frequent nummber
+# print display
