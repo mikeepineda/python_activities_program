863d5ba58fe38368b6520083358066e3055b58f2
incessmikee <incess.mikee@gmail.com>
2025-03-13 13:40:05 +0800

    code complete

--- batch_2/prog_01_print_smaller_number.py
+++ batch_2/prog_01_print_smaller_number.py
@@ -1,5 +1,8 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number. 
 
 # ask user two input
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 
 # print smaller number
+print("The smaller number is",  min(num1,num2))
