af1b165c9ecafee1858d90f2c05678eeff22d577
incessmikee <incess.mikee@gmail.com>
2025-03-13 18:33:19 +0800

    check if num is seen

--- batch_3/prog_02_display_first_entry_if_duplicate.py
+++ batch_3/prog_02_display_first_entry_if_duplicate.py
@@ -1,13 +1,13 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
-# store user input
-# set to track seen numbers
+numbers = [] # store user input
+seen = set() # set to track seen numbers
 # ask user to input 10 numbers
 for i in range (10): 
     num = int(input(f"Enter your number {i +1: }"))
-# check if number is already displayed
-# mark as seen
-# store in list
+    if num not in seen: # check if number is already displayed
+        seen.add(num)   # mark as seen
+    numbers.append(num) # store in list
 # display result
 # create an empty set called displayed to track prints
 # mark as printed
