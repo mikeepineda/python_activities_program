2c375d73d0c4378444e67e096369f99458eb8bcd
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:26:52 +0800

    create loop

--- batch_4/prog_05_display_average.py
+++ batch_4/prog_05_display_average.py
@@ -1,7 +1,16 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+numbers = []
+
 # ask user to input
 # create loop
-# break if the input is invalid
+while True:
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        numbers.append(num)
+    except ValueError:
+        break # break if the input is invalid
+
+
 # calculate
 # display the average
