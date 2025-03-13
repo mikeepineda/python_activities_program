f063091d3d2fd0cb2dedba39471bb1557b5793f8
incessmikee <incess.mikee@gmail.com>
2025-03-13 18:27:52 +0800

    prog 02: pseudocode

---
+++ batch_3/prog_02_display_first_entry_if_duplicate.py
@@ -0,0 +1,11 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# store user input
+# set to track seen numbers
+# ask user to input 10 numbers
+# check if number is already displayed
+# mark as seen
+# store in list
+# display result
+# create an empty set called displayed to track prints
+# mark as printed
