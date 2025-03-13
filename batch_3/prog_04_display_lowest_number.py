df91b774a7a6d15cab8e836e9aac3ce37531348a
incessmikee <incess.mikee@gmail.com>
2025-03-13 19:06:34 +0800

    prog 04: pseudocode

---
+++ batch_3/prog_04_display_lowest_number.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# add variable to store lowest number
+# ask for user's input
+# update lowest number
+# break if the input is invalid
