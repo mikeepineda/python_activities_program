47501e9fa171a9db8ee24ce8575e65357a1ca804
incessmikee <incess.mikee@gmail.com>
2025-03-13 15:04:39 +0800

    code complete

--- batch_2/prog_10_print_numbers_between.py
+++ batch_2/prog_10_print_numbers_between.py
@@ -1,7 +1,12 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # ask user two inputs
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
 # determine the range
 # find the smaller number and add 1, and find the larger number
-# loop 
+for i in range (min(num1,num2) + 1,  max(num1,num2)):
 
+# loop 
+    print (i)
