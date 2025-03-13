018c1e9f1d12c12d76479fa19476d7af7157de16
incessmikee <incess.mikee@gmail.com>
2025-03-13 19:10:45 +0800

    ask for user's input

--- batch_3/prog_04_display_lowest_number.py
+++ batch_3/prog_04_display_lowest_number.py
@@ -1,6 +1,10 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # add variable to store lowest number
+lowest = None 
+
 # ask for user's input
+num = int(input("Enter a number: "))
+
 # update lowest number
 # break if the input is invalid
