9e990c904e8589a3ad9606defb9cd4a83a57d0e8
incessmikee <incess.mikee@gmail.com>
2025-03-13 13:50:14 +0800

    code complete

--- batch_2/prog_03_print_difference.py
+++ batch_2/prog_03_print_difference.py
@@ -1,5 +1,8 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # ask user two inputs
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 
 # print difference
+print ("The difference is", num1 - num2)
