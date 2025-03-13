be3e789507f4209b01215c8953e691b1600e6581
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:09:52 +0800

    prog 03 pseudocode

---
+++ batch_4/prog_03_display_highest_number.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+# ask for user's input
+# create loop
+# break if the input is invalid
+# display the highest number
