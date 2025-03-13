779df4a3561ff51a52f0394bec54d89f212f3bb9
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:05:09 +0800

    code complete

--- batch_2/prog_05_print_remainder.py
+++ batch_2/prog_05_print_remainder.py
@@ -1,5 +1,9 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 # ask user 2 inputs
+num1 = int(input("Input your first number: "))
+num2 = int(input("Input your second number: "))
+result = num1 % num2
 
 # print remainder
+print ("The remainder is ", result)
