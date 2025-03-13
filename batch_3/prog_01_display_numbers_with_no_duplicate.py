60d82fada9e91ed9cbd976fab59c043dc55b45c5
incessmikee <incess.mikee@gmail.com>
2025-03-13 18:01:15 +0800

    prog 01: pseudocode

---
+++ batch_3/prog_01_display_numbers_with_no_duplicate.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# store user input
+# ask user to input 10 numbers
+# find numbers that don't have duplicate
+# display result
