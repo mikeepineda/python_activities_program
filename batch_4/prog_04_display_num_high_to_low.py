4c6ef01b499865e408aa976a2fe9d88cfdda829c
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:19:53 +0800

    create loop

--- batch_4/prog_04_display_num_high_to_low.py
+++ batch_4/prog_04_display_num_high_to_low.py
@@ -1,6 +1,15 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
+numbers = []
+
 # create loop
+while True:
+
 # ask user to input number
-# break loop if the input's invalid
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        numbers.append(num)
+    except ValueError:
+        break  # break loop if the input's invalid
+
 # sort and display numbers from highest to lowest
