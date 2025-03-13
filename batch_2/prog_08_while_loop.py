890b120a52ef0a32fbc8b581cd4a19b4282b92d7
incessmikee <incess.mikee@gmail.com>
2025-03-13 14:42:38 +0800

    prog 08 psuedocode

---
+++ batch_2/prog_08_while_loop.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+# start from the first odd number
+# print the odd number
+# move to the next odd number
