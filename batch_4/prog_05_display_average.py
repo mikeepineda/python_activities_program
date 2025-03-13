60406960f7a80b40beab7bafcb2780dca20b4c34
incessmikee <incess.mikee@gmail.com>
2025-03-13 23:25:11 +0800

    prog 05 pseudocode

---
+++ batch_4/prog_05_display_average.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# ask user to input
+# create loop
+# break if the input is invalid
+# calculate
+# display the average
